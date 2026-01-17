8a6cb7c467f2b4b7c2010e974204f79f1b5ed526
AndiswaMatai <andiswacebekhulu1@gmail.com>
2026-01-17 21:05:18 +0200

    Delete azure-incremental-pipeline/scripts directory

--- azure-incremental-pipeline/scripts/watermark_control.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE WatermarkControl (
-    PipelineName VARCHAR(100),
-    LastProcessedTimestamp DATETIME
-);
-
-INSERT INTO WatermarkControl
-VALUES ('TripsIncrementalPipeline', '2024-01-01 00:00:00');
-
