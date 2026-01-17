241665cdfdb5a64f425098097d7437e66f254c68
AndiswaMatai <andiswacebekhulu1@gmail.com>
2026-01-17 21:04:46 +0200

    Create watermark_control.sql

---
+++ scripts/watermark_control.sql
@@ -0,0 +1,7 @@
+CREATE TABLE WatermarkControl (
+    PipelineName VARCHAR(100),
+    LastProcessedTimestamp DATETIME
+);
+
+INSERT INTO WatermarkControl
+VALUES ('TripsIncrementalPipeline', '2024-01-01 00:00:00');
